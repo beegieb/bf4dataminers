08e52a44a170879eb17e2e7c9afd11c17d516a2d
Miroslaw Horbal <miroslaw@gmail.com>
2014-06-09 01:25:19 -0400

    Added comment/docstring

--- minePlayerStats.R
+++ minePlayerStats.R
@@ -1,4 +1,20 @@
 minePlayerStatsFromBF4Stats <- function(playerData) {
+  #
+  # Mines player statistics from BF4stats.com's API
+  #
+  # playerData is a dataframe containing columns name and plat
+  # name - contains the personaNames of the players to be mined 
+  # plat - contains the platform's integer identifier (2-Xbox, 4-PS3)
+  #
+  # Returns a character vector equal length to the number
+  # of rows in playerData. Each element contains either
+  # the raw JSON output from the API or the string "failed"
+  #
+  # If something goes wrong in the middle of this function, a
+  # temp file is maintained in the working directory with
+  # the name "_players.tmp". This file is removed at the end
+  # of the function call
+  #
   bf4statsurl <- "http://api.bf4stats.com/api/playerInfo"
   opt <- "stats,extra,vehicleCategory,weaponCategory"
   player.json <- character(nrow(playerData))
